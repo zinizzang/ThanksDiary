6b712ed16fb5b3c22d2d3c3cd26a51c823efa478
zinizzang <song0319s@gmail.com>
2025-08-09 12:01:09 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+
+const CACHE_NAME = 'ttc-cache-emotional-v1-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>{
+      if(k!==CACHE_NAME) return caches.delete(k);
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp)=>{
+      return resp || fetch(event.request).then((response)=>{
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(event.request, copy));
+        return response;
+      }).catch(()=>resp);
+    })
+  );
+});
