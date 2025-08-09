995552b5b071aa3989d930f999bac60481e70d94
zinizzang <song0319s@gmail.com>
2025-08-09 15:31:08 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'thanksdiary-cache-v2-2';
+const CACHE_NAME = 'thanksdiary-cache-v2-3';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k))))); self.clients.claim(); });
